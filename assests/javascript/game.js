b43847142c7df96bb83f793e8cd52dd36c3638d6
Lap Larry Nguyen <larrylapnguyen@gmail.com>
2019-01-02 11:46:09 -0800

    finish hw

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,35 +1,148 @@
+/*
+
+!!!!! I can't figure out why the first round of the game does not work properly, but after you win or lose the first round it runs perfectly !!!
+
+*/
+
 //Word Generation
-var words = ["dog", "rhinoceros", "gorrilla", "chimpanzee", "lion", "cat", "dolphin"];
-var randomWord = Math.floor(Math.random()*words.length);
-var answer = words[randomWord];
-var wordLength = answer.length;
-var display = [wordLength];
-var winner = wordLength;
-var letters = answer.split(' ');
-var outty = " ";
-
-//Counters
+var words = ["snowman", "snowball", "hail", "snow", "blizzard", "frozen", "icicle", "cold", "frost"];
+var randomWordNum;
+var possLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var word; 
+var guessedLetters = [];
+var chosenWord = [];
+var blanks = [];
+var lives = 10;
+var b; //to print out blanks without  commas
 var score = 0;
-var losses = 0;
-var guessesLeft = 10;
+randWord();
+var letters = word.length;
+var already = "";
+chosenWord = word.split('');
 
-
-for (let i = 0; i < answer.length; i++) {
-   display[i] = "_ ";
-    outty = outty + display[i];
+function randWord() {
+      randomWordNum = Math.floor(Math.random() * words.length);
+      word = words[randomWordNum];
 }
-document.getElementById("spots").innerHTML = outty;
-output = "";
 
-console.log(answer);
+function blanky() {
+      //displays the blanks for each letter of the word
+      for (let i = 0; i < chosenWord.length; i++) {
+            blanks.push("_ ");
+            b = document.getElementById("word");
+            b.innerHTML = blanks.join(" ");
+            // document.getElementById("word").innerHTML = blanks;
+      }
+}
 
-//Input
-document.onkeypress=function(event){
-    var userGuess = event.key;
-    console.log(userGuess);
-    if (userGuess == ) {
-    
-    }
-};
+function lost() {
+      if (lives == 0) {
+            alert("You lose!");
+            lives = 10;
+            letters = word.length;
+            guessedLetters = [];
+            chosenWord = word.split('');
+            blanks = [];
+            blanky();
+      }
+}
+
+function restart() {
+      randWord();
+      letters = word.length;
+      guessedLetters = [];
+      chosenWord = word.split('');
+      blanks = [];
+      blanky();
+      console.log(blanks.length + " Blanks");
+      console.log(chosenWord + " New Chosen word");
+      lives = 10;
 
 
+
+
+
+      b.innerHTML = blanks.join(" ");
+      document.getElementById("yourGuess").innerHTML = guessedLetters;
+      document.getElementById("lettersLeft").innerHTML = letters;
+      document.getElementById("livesLeft").innerHTML = lives;
+}
+
+function game() {
+      //takes user input and checks if letter or already guessed
+      document.onkeyup = function (event) {
+            var userGuess = event.key;
+
+            if (possLetters.includes(userGuess)) {
+
+                  for (let j = 0; j < word.length; j++) {
+                        if (chosenWord[j] == userGuess) {
+                              blanks[j] = userGuess;
+                              console.log(blanks);
+                              if (letters > 0 && !guessedLetters.includes(userGuess)) {
+                                    letters--;
+                              }
+                        }
+                  }
+                  if (!word.includes(userGuess) && !guessedLetters.includes(userGuess)) {
+                        lives--;
+                        lost();
+                  }
+
+                  if (guessedLetters.includes(userGuess)) {
+                        already = "You already tried that!"
+                        document.getElementById("already").innerHTML = already;
+
+                  } else {
+                        guessedLetters.push(userGuess);
+                        already = "";
+                        document.getElementById("already").innerHTML = already;
+                  }
+            }
+            if (letters == 0) {
+                  score++;
+                  document.getElementById("youWin").innerHTML = score;
+                  console.log(score + " is current score");
+                  randWord();
+                  letters = word.length;
+                  guessedLetters = [];
+                  chosenWord = word.split('');
+                  blanks = [];
+                  blanky();
+                  console.log(blanks .length+ " Blanks");
+                  console.log(chosenWord + " is the new word");
+                  lives = 10;
+            }
+
+
+            //Write to html
+            b.innerHTML = blanks.join(" ");
+            document.getElementById("yourGuess").innerHTML = guessedLetters;
+            document.getElementById("lettersLeft").innerHTML = letters;
+            document.getElementById("livesLeft").innerHTML = lives;
+      }
+}
+
+if (letters > 0) {
+      randWord();
+      blanky();
+      game();
+      document.getElementById("youWin").innerHTML = score;
+
+}
+
+b.innerHTML = blanks.join(" ");
+document.getElementById("yourGuess").innerHTML = guessedLetters;
+document.getElementById("lettersLeft").innerHTML = letters;
+document.getElementById("livesLeft").innerHTML = lives;
+
+
+var x = document.getElementById("myAudio");
+
+function playAudio() {
+      x.play();
+}
+
+function pauseAudio() {
+      x.pause();
+}
